458e257af5cc20c114567e26373a392f6273531d
Cristian Torres <cristian.torres19@hotmail.com>
2025-10-19 15:28:22 -0500

    Remove middleware file and simplify Auth0 client configuration in auth0.ts by eliminating unnecessary parameters.

--- src/app/lib/auth0.ts
+++ src/app/lib/auth0.ts
@@ -1,9 +1,3 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-export const auth0 = new Auth0Client({
-  secret: process.env.AUTH0_SECRET,
-  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
-  baseURL: process.env.APP_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-});
+export const auth0 = new Auth0Client();
